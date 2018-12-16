4f24eac98d31ed2305eec3678e7bfe49a5807660
Administrator <Administrator@ASI-1>
2018-12-16 08:58:00 -0600

    moving code to Remore rep

---
+++ FREECRM_MAVEN_PROJECT/src/test/java/com/FREECRM/Login/LoginTestCase.java
@@ -0,0 +1,12 @@
+package com.FREECRM.Login;
+
+import org.testng.annotations.Test;
+
+public class LoginTestCase {
+	
+	@Test
+	public void LoginFreeCRM() {
+		System.out.println("this is test case");
+	}
+
+}
