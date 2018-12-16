fdd98a328090a83b450a7c217e7c039c0f1fb53c
smuvw <srinivasarao_muvva@yahoo.com>
2018-12-16 09:03:30 -0600

    Update LoginTestCase.java

--- FREECRM_MAVEN_PROJECT/src/test/java/com/FREECRM/Login/LoginTestCase.java
+++ FREECRM_MAVEN_PROJECT/src/test/java/com/FREECRM/Login/LoginTestCase.java
@@ -8,6 +8,7 @@ public class LoginTestCase {
 	public void LoginFreeCRM() {
 		System.out.println("this is test case");
 		System.out.println("welcome to java");
+		System.out.println("welcome to Selenium");
 	}
 
 }
